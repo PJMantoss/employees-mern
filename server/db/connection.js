3bc2c1519ede569cade069c71a4ed7e7e39052ed
PJMantoss <joelptoss@gmail.com>
2022-01-01 18:47:20 +0100

    update connection.js

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,1 +1,14 @@
 const { MongoClient } = require("mongodb");
+const Db = process.env.ATLAS_URI;
+const client = new MongoClient(Db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+let _db;
+
+module.exports = {
+    connectToServer: function(callback){
+        client.connect(() => {})
+    }
+}
