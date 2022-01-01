7c15cdcbe01051707730808294e0e339624cb999
PJMantoss <joelptoss@gmail.com>
2022-01-01 18:09:50 +0100

    update server.js

--- server/server.js
+++ server/server.js
@@ -7,13 +7,16 @@ require("dotenv").config({ path: "./config.env" })
 
 const port = process.env.PORT || 5000;
 
-app.use(express.json());
+app.use(cors());
 
-app.use(cors())
+app.use(express.json());
 
 app.use(require("./routes/record"));
 
 //Get driver connection
 const dbo = require("./db/connection");
 
-app.use()
+app.listen(port, () => {
+    //perform a db connection whent server starts
+    dbo.connectToServer()
+});
