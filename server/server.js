73c55eae153f239bded5de731cf772cf7bbcbe33
PJMantoss <joelptoss@gmail.com>
2022-01-01 18:06:26 +0100

    update server.js

--- server/server.js
+++ server/server.js
@@ -3,8 +3,17 @@ const app = express();
 
 const cors = require("cors");
 
+require("dotenv").config({ path: "./config.env" })
+
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
 app.use(cors())
+
+app.use(require("./routes/record"));
+
+//Get driver connection
+const dbo = require("./db/connection");
+
+app.use()
